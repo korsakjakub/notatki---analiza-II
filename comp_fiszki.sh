8256479b539221bd62b4913002f5e3ebc72f0afa
Jakub Korsak <korsakuba@gmail.com>
2019-09-10 13:08:06 +0200

    dużo dużo

--- comp_fiszki.sh
+++ comp_fiszki.sh
@@ -1,4 +1,24 @@
+#!/bin/sh
+
+checkArguments() {
+    if [ $# -eq 0 ]; then
+        echo "USAGE: comp_fiszki.sh [definicja/tw/stw]"
+        return 0;
+    else
+        return 1;
+    fi
+}
+
+if [ checkArguments -eq 1 ]; then
+    echo "done"
+else
+    echo "not done"
+fi
+
+echo "hello"
+
 for i in tex/*.tex;
 do
-    sh parser.sh "$i" definicja fiszki/definicje.tex;
+    sh parser.sh "$i" begin{$1} fiszki/$1.tex;
 done
+
